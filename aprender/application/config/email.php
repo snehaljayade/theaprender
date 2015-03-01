bd19414ef5f8130579171ce1906774cc4d5d24c2
snehaljayade <snehal.jayade@gmail.com>
2015-03-01 16:08:05 +0530

    change

--- aprender/application/config/email.php
+++ aprender/application/config/email.php
@@ -6,7 +6,7 @@ $config = Array(
 			'smtp_host' => 'ssl://smtp.googlemail.com',
 			'smtp_port' => 465,
 			'smtp_user' => 'theaprender@gmail.com',
-			'smtp_pass' => 'engineer2015',
+			'smtp_pass' => 'xxxx',
 			'mailtype'  => 'html', 
 			'charset'   => 'iso-8859-1'
 		);
